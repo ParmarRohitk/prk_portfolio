85b9fd22605f11a05e1b2425aa5d85225f822bee
Parmar Rohit k. <86903998+ParmarRohitk@users.noreply.github.com>
2025-01-14 22:48:46 +0530

    add favicon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -20,6 +20,25 @@ export default function App({ Component, pageProps }) {
         {/* <link rel="preconnect" href="https://api.openweathermap.org"></link> */}
         {/* <link rel="preconnect" href="https://cdnjs.cloudflare.com"></link> */}
         <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        ></link>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        ></link>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        ></link>
+        <link rel="manifest" href="/site.webmanifest"></link>
       </Head>
       <main
         className={`${montserrat.variable} font-mont  bg-light dark:bg-dark w-full min-h-screen h-full`}
