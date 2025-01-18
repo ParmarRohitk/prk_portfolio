530a140db97921ca0f8a1df179f42596cd8b757a
Parmar Rohit k. <86903998+ParmarRohitk@users.noreply.github.com>
2025-01-18 16:32:02 +0530

    update about page content

---
+++ src/components/Educations.js
@@ -0,0 +1,114 @@
+import React, { useRef } from "react";
+import { motion, useScroll } from "framer-motion";
+import LiIcon from "./LiIcon";
+import Link from "next/link";
+
+const Educations = ({
+  position,
+  company,
+  companyLink,
+  time,
+  address,
+  work,
+}) => {
+  const ref = useRef(null);
+  return (
+    <li
+      ref={ref}
+      className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-start justify-between md:w-[80%]"
+    >
+      <LiIcon reference={ref} />
+      <motion.div
+        initial={{ y: 50 }}
+        whileInView={{ y: 0 }}
+        transition={{ duration: 0.5, type: "spring" }}
+      >
+        <h3 className="capitalize font-bold text-2xl sm:text-xl xs:text-lg">
+          {position}{" "}
+          <a
+            className="capitalize text-slideGreen dark:text-primaryDark"
+            href={companyLink}
+            target={"_blank"}
+          >
+            @{company}
+          </a>
+        </h3>
+        <span className="capitalize text-dark/75 font-medium dark:text-light/50 xs:text-sm">
+          {time} | {address}
+        </span>
+        <p className="font-medium w-full md:text-sm"> {work}</p>
+      </motion.div>
+    </li>
+  );
+};
+
+const Experience = () => {
+  const ref = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start end", "center start"],
+  });
+
+  return (
+    <div className="my-64">
+      <h2 className="font-bold text-8xl mb-32 w-full text-center md:text-6xl xs:text-4xl md:mb-16">
+        Educations
+      </h2>
+
+      <div ref={ref} className="relative w-[75%] mx-auto lg:w-[90%] md:w-full">
+        <motion.div
+          className="absolute left-9 top-0 w-[4px] md:w-[2px] md:left-[30px] xs:left-[20px] h-full bg-primaryDark shadow-3xl 
+            origin-top  dark:bg-primaryDark dark:shadow-3xl"
+          style={{ scaleY: scrollYProgress }}
+        />
+
+        <ul className="w-full flex flex-col items-start justify-between ml-4 xs:ml-2">
+          <Details
+            position="Bachelor of Computer Applications (BCA)"
+            company="Veer Narmad South Gujarat University"
+            time="2020-2023"
+            address="Computer science"
+            companyLink=""
+            work="As a software engineer into web development and programming, interested in PHP, JavaScript and Python, HTML & CSS, web frameworks including Bootstrap and Python’s Flask, and NoSQL databases such as MongoDB. To be precise, motivated on developing engaging web interfaces and never ceasing to extend my knowledge in technology."
+          />
+
+          <Details
+            position="Master of Computer Applications (MCA)"
+            company="Jain (Deemed-to-be University), Bangalore"
+            time=" 2023 – 2025"
+            address="Computer Science and IT"
+            companyLink=""
+            work="Having majored in Computer Science and IT, I am developing competencies in higher level programming paradigms, data structures and object oriented programming paradigms while developing personalities, communication, personality development and technical problem solving skills."
+          />
+        </ul>
+      </div>
+      <div className="mt-40 flex items-center justify-between gap-3 grid-cols-2">
+        <Link
+          href="/projects/"
+          target={"_self"}
+          className={`flex items-center rounded-lg border-2 border-solid bg-light p-2.5 px-6 text-lg font-semibold
+            capitalize text-dark hover:border-light hover:bg-dark hover:text-light 
+            dark:bg-dark dark:text-light dark:hover:bg-light dark:hover:text-dark
+            md:p-2 md:px-4 md:text-base
+             `}
+        >
+          View Projects
+        </Link>
+        <Link
+          href="/articles/"
+          target={"_self"}
+          className={`flex items-center rounded-lg border-2 border-solid bg-dark p-2.5 px-6 text-lg font-semibold
+            capitalize text-light hover:border-dark hover:bg-transparent hover:text-dark 
+            dark:bg-light dark:text-dark dark:hover:border-light dark:hover:bg-dark dark:hover:text-light
+            md:p-2 md:px-4 md:text-base
+             `}
+        >
+          View Articles
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Educations;
--- src/pages/about.js
+++ src/pages/about.js
@@ -9,6 +9,8 @@ import Experience from "@/components/Experience";
 import AnimatedText from "@/components/AnimatedText";
 import TransitionEffect from "@/components/TransitionEffect";
 import { HireMe2 } from "@/components/HireMe2";
+import profilePic from "../../public/images/profile/prk.png";
+import Educations from "@/components/Educations";
 
 function AnimatedNumberFramerMotion({ value }) {
   const ref = useRef(null);
@@ -51,7 +53,7 @@ export default function About() {
       >
         <Layout className="pt-16">
           <AnimatedText
-            text="Let Passion Lead Your Purpose 🔥"
+            text="Follow Your Passion’s Path 🔥"
             className="mb-16 !text-8xl !leading-tight lg:!text-7xl sm:!text-6xl xs:!text-4xl sm:mb-8"
           />
 
@@ -64,27 +66,31 @@ export default function About() {
                 BIOGRAPHY
               </h2>
               <p className="font-medium ">
-                I'm Rohit, a dedicated web developer at Stellar Innovations,
-                where I specialize in creating dynamic and user-centric web
-                experiences. With over a decade of experience in the industry, I
-                have honed my skills in both front-end and back-end
-                technologies, allowing me to deliver responsive and
-                high-performance websites and applications.
+                Hello there, my name is Parmar Rohit K. I am a fully dedicated
+                Full Stack Developer , pursuing MCA in the field of computer
+                science & IT . Training in web development for over 1 year and 8
+                months, I enjoy being part of a team that creating user-centric
+                interactive applications that introduce elements of creativity
+                with functionality.
               </p>
               <p className="my-4 font-medium">
-                At Stellar Innovations, I've had the opportunity to work on
-                numerous high-profile projects that have challenged and expanded
-                my expertise. My technical skill set includes HTML, CSS,
-                JavaScript, React, and Node.js, among other technologies. One of
-                my notable projects involved developing a comprehensive
-                e-commerce platform that streamlined the user experience and
-                significantly boosted the client's sales. Another project I’m
-                particularly proud of was creating an interactive web
-                application for a major event, which received widespread acclaim
-                for its intuitive design and seamless performance.
+                These experiences have contributed to my journey in building the
+                tech industry from the frontend to end with a core expertise in
+                backend architecture. Designing interfaces and creating
+                exceptional visual images to optimizing intricate back end
+                solutions and delivering fabulous value added and efficient user
+                specific digital solutions is what I enjoy most. In addition to
+                coding, I have passion in solving problems and learning. In any
+                web development project, including a search engine optimization
+                project or calling APIs to improve the website, I always seek to
+                provide other solutions that are practical and high value in
+                terms of outcome.
               </p>
               <p className="my-4 font-medium">
-                P.s I'm a big fan of anime and not a real person ✌️.
+                When I am not coding I like to watch anime because it helps
+                generate as many ideas as possible often leading to thinking out
+                of the box. Join us to All Ideas INTO Reality or, as the acronym
+                suggests.
               </p>
             </div>
             <div
@@ -101,7 +107,7 @@ export default function About() {
               <Image
                 className="h-auto w-full rounded-2xl border-2 border-solid border-dark"
                 priority={true}
-                src={profile}
+                src={profilePic}
                 alt="PRK"
                 sizes="(max-width: 768px) 100vw,
               (max-width: 1200px) 50vw,
@@ -114,7 +120,7 @@ export default function About() {
             >
               <div className="flex flex-col items-end justify-center xl:items-center">
                 <span className="inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl">
-                  <AnimatedNumberFramerMotion value={1461} />+
+                  <AnimatedNumberFramerMotion value={1825} />+
                 </span>
                 <h3
                   className="mb-4 text-xl font-medium capitalize text-dark/75 dark:text-light/75 
@@ -126,7 +132,7 @@ export default function About() {
 
               <div className="flex flex-col items-end justify-center xl:items-center">
                 <span className="inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl">
-                  <AnimatedNumberFramerMotion value={500} />
+                  <AnimatedNumberFramerMotion value={645} />
                 </span>
                 <h3
                   className="mb-4 text-xl font-medium capitalize text-dark/75 dark:text-light/75 
@@ -138,7 +144,7 @@ export default function About() {
 
               <div className="flex flex-col items-end justify-center xl:items-center">
                 <span className="inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl">
-                  <AnimatedNumberFramerMotion value={499} />
+                  <AnimatedNumberFramerMotion value={389} />
                 </span>
                 <h3
                   className="mb-4 text-xl font-medium capitalize text-dark/75 dark:text-light/75 
@@ -153,6 +159,7 @@ export default function About() {
 
           <Skills />
           <Experience />
+          {/* <Educations /> */}
         </Layout>
       </main>
     </>
